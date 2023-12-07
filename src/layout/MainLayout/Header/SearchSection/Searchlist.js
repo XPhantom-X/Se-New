06aa106ec777a9a0bb280069ca92d0037490b1bf
Usman960 <mu943498@gmail.com>
2023-12-07 20:03:03 +0500

    Search mapping implemented

--- src/layout/MainLayout/Header/SearchSection/Searchlist.js
+++ src/layout/MainLayout/Header/SearchSection/Searchlist.js
@@ -1,27 +1,22 @@
-import { Box } from "@mui/system";
+import { Typography,Box } from "@mui/material";
 import React from "react";
-// import {searchResults} from "./index.js";
 import { useLocation } from "react-router";
 
 const Searchlist = () => {
-   const loc = useLocation();
-   console.log(loc.state.res)
+  const loc = useLocation();
+  console.log(loc.state.res)
 
-    return (
-   
+  return (
     <div>
-    <h1>{loc.state.res[0].firstName}</h1>
-    <h1>{loc.state.res[0].lastName}</h1>
-    <h1>{loc.state.res[0].department}</h1> 
-    
-    
-    <h1>{loc.state.res[1].firstName}</h1>
-    <h1>{loc.state.res[1].lastName}</h1>
-    <h1>{loc.state.res[1].department}</h1>
-   
+      {loc.state.res.map((item, index) => (
+        <Box style={{ display:"flex", flexDirection:"column", marginBottom:"10px" }} key={index}>
+          <Typography variant="h4">{item.firstName} {item.lastName}</Typography>
+          <Typography variant="h6">{item.department} | {item.officeHours}</Typography>
+          <Typography variant="h5">{item.coursesAssigned}</Typography>
+        </Box>
+      ))}
     </div>
-
-      );
+  );
 }
- 
+
 export default Searchlist;
